301b3e7258be779e3968b6d1c15f4f4fa6da52df
pjdip <pjdip1@gmail.com>
2021-01-11 12:08:53 -0500

    working on server.js

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// NPM package dependencies
+var express = require("express");
+
+// Creating an express server
+var app = express();
+
+// Establishing an initial port
+var PORT = process.env.PORT || 8080;
+
+// Middleware for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing
+
+
+// Listener
+app.listen(PORT, function() {
+    console.log("Note app listening on PORT: " + PORT);
+});
