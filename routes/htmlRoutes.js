bcf4772616751cd82c230df93e535256de5da951
pjdip <pjdip1@gmail.com>
2021-01-12 12:35:27 -0500

    adding '/' html route for heroku deployment in accordance with the deployment guide

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,16 @@ const path = require("path");
 module.exports = function(app) {
 
     // HTML GET requests for each visitable page
-    app.get("/notes", function(req,res) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "..public/index.html"));
+    });
+    
+    app.get("/notes", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     // * gives the default route when no matching route is found
-    app.get("*", function(req, res) {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
